79db9fdbb0c7b8dda8ab1190047fb917d9ec2abf
R r <rommuelrivera3@gmail.com>
2025-03-22 01:42:13 +0800

    Figured from main.py that ValueError is not necessarily include int inputs (numbers are type-converted to str)

--- Assignments-Exercises/Python-4_Functions/ass4.py
+++ Assignments-Exercises/Python-4_Functions/ass4.py
@@ -1,12 +1,12 @@
-def create_greeting(name):
+def create_greeting(name: str):
+    #returns template 
+    return f"Hello, {name}!, welcome to the GDG Web Development Team! You're doing great, and I truly believe that someday you'll be an amazing developer. Life may feel challenging right now, and programming can be overwhelming at times, but remember, all your hard work will pay off in the end. Keep pushing forward, you're on the right path!"
 
-    print(f"Hello, {name}!, welcome to the GDG Web Development Team! You're doing great, and I truly believe that someday you'll be an amazing developer. Life may feel challenging right now, and programming can be overwhelming at times, but remember, all your hard work will pay off in the end. Keep pushing forward, you're on the right path!")
-
-
+ 
 try:
-     name = input("Enter your name: ")
-     create_greeting(name)
+    user_name = input("Enter your name: ") #user inputs name 
+    print(create_greeting(user_name))
     
 except ValueError:
-    print("Invalid input. Please enter a string.")
+    print("Invalid input. Please enter a valid name.")
 
