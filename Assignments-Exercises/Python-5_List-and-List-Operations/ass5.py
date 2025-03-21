446ac30886d15b4424d6cb60e312098a4d20ad3c
R r <rommuelrivera3@gmail.com>
2025-03-22 01:42:38 +0800

    initial commit

---
+++ Assignments-Exercises/Python-5_List-and-List-Operations/ass5.py
@@ -0,0 +1,8 @@
+list = [2, 4, 9, 6, 8, 22, 11, 1, 7, 5]
+print(f"Original list: {list}")
+list.append(21)
+print(f"After adding an element: {list}")
+list.remove(22)
+print(f"After removing an element: {list}")
+list.sort()
+print(f"Sorted in ascending order: {list}")
